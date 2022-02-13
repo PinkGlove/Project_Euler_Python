67cc3e5fe7f742b76a73dd21417ec6230ebf7748
pinkglove <z.guo-1@student.utwente.nl>
2022-02-13 14:26:08 +0100

    q8, q9 finished

---
+++ src/q1_q50/q9.py
@@ -0,0 +1,9 @@
+import math
+
+for a in range(1, 1000):
+    for b in range(a, 1000):
+        c = 1000 - a - b
+        if c >= 0:
+            if a ** 2 + b ** 2 == c ** 2:
+                print(a * b * c, a, b, c)
+                break
